e3d3ef6c16a866b0c66e90e71cd9b0a026d1799d
Selim Hassairi <f00430t@m210-linux-09.thayer.dartmouth.edu>
2019-10-17 19:17:48 -0400

    the hash part is not wroking

--- tse/crawler/crawler.c
+++ tse/crawler/crawler.c
@@ -15,6 +15,7 @@
 #include <hash.h>
 #include <webpage.h>
 #include <stdbool.h>
+#include <string.h>
 
 /*
 int main() {
@@ -57,6 +58,17 @@ void print_webpage(void *page) {
 	printf("URL: %s\n", webpage_getURL(tmp));
 }
 
+bool searchURL(void* elementp, const void* searchkeyp){
+	char *t_url;
+	strcpy(t_url,(char*) elementp);
+	char *c_url;
+	strcpy(c_url,(char*) searchkeyp);
+	printf("COMPARING %s to %s \n",t_url,c_url);
+	if (strcmp(t_url,c_url)==0) {
+		return true;
+	}
+	return false;
+}
 
 int main() {
 	char seed_url[300] = "https://thayer.github.io/engs50/";
@@ -69,16 +81,23 @@ int main() {
 	int pos = 0;
 	char *res;
 
-	queue_t *webpages = qopen();
+	queue_t *webpages = qopen(); // to help the breadth first exploration of the website
+	hashtable_t *hash = hopen(10); //for visited webpages
 	
 	while ( (pos=webpage_getNextURL(page,pos,&res)) > 0) {
 		//char urltype[30];
 		//urltype = IsInternalURL(res) ? "Internal" : "External";
 		printf("Found url of type  %s  : ' %s'\n", IsInternalURL(res) ? "Internal" : "External", res);
-
+		// Check if in Internal
 		if (IsInternalURL(res)) {
-			webpage_t *tmp_page = webpage_new(res, 1, NULL);
-			qput(webpages, tmp_page);
+			// Check if it has been visited (in the hash)
+			if (hsearch(hash,searchURL,res,sizeof(res))==NULL){ 
+					webpage_t *tmp_page = webpage_new(res, 1, NULL);
+					// Add to visited pages
+					hput(hash,res,res,sizeof(res));
+					// Add to the breadth first queue
+					qput(webpages, tmp_page);
+				}
 		 }
 		
 		free(res);
